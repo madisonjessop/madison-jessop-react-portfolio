d3a85b75aa70a18fea0919028619ca1725c2f642
madisonjessop <madisonrjessop@gmail.com>
2019-03-04 16:31:06 -0700

    Added auth/login page, and updated some main page styling

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -58,14 +58,12 @@ export default class PortfolioContainer extends Component {
     }
 
     return (
-      <div>
-        <h2>{this.state.pageTitle}</h2>
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+        <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+        <button className="btn" onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
 
-        <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-        <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-        <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
-        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+        {this.portfolioItems()}
       </div>
     );
   }
